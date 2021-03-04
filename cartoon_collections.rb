44a659b54ebe06722ab0cfbc51c095940fce04b2
sumeikom <sumeikom@gmail.com>
2021-03-04 02:11:58 -0500

    commit tree 0faeacd29045ee2def68a349433fcb836540d791

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  place = 1
+  array.each do |name|
+    puts "#{place}. #{name}"
+    place += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect { |x| x.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  if array.size > 4
+    false
+  else
+    true
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find {|name| cheese_types.include?(name)}
 end
